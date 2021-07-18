f3dd05b24e41db2637bd1f598bece546efea9bce
derder <derder9527@gmail.com>
2021-07-18 21:18:39 +0800

    first commit

---
+++ src/main/java/derder9527/idv/mcspringbootservices/McSpringbootServicesApplication.java
@@ -0,0 +1,13 @@
+package derder9527.idv.mcspringbootservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McSpringbootServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McSpringbootServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/derder9527/idv/mcspringbootservices/McSpringbootServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package derder9527.idv.mcspringbootservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McSpringbootServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
